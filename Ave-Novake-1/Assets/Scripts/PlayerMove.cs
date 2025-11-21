ce54d44b4cbabde1e556eb3d13fb6cb626b0d000
Narisse-H <juzi-ex@outlook.com>
2025-11-21 15:18:00 +0800

    测试用项目

---
+++ Ave-Novake-1/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float player_speed;
+    public Rigidbody2D player_rb;
+    public Collider2D player_coll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_rb = GetComponent<Rigidbody2D>();
+        player_coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+    void PlayerMove()
+    {
+        float horizontal_num = Input.GetAxis("Horizontal");
+        float face = Input.GetAxisRaw("Horizontal");
+        if (face != 0)
+        {
+            transform.localScale = new Vector3(-face, transform.localScale.y, transform.localScale.z);
+        }
+        player_rb.velocity = new Vector2(player_speed * horizontal_num, player_rb.velocity.y);
+    }
+}
