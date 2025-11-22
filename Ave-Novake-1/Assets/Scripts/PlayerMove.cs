98fea9eb53e9866c5187605273f5342f4b093dbb
Narisse-H <juzi-ex@outlook.com>
2025-11-22 11:34:06 +0800

    add animation

--- Ave-Novake-1/Assets/Scripts/PlayerMove.cs
+++ Ave-Novake-1/Assets/Scripts/PlayerMove.cs
@@ -7,12 +7,14 @@ public class NewBehaviourScript : MonoBehaviour
     public float player_speed;
     public Rigidbody2D player_rb;
     public Collider2D player_coll;
+    public Animator Player_anim;
 
     // Start is called before the first frame update
     void Start()
     {
         player_rb = GetComponent<Rigidbody2D>();
         player_coll = GetComponent<Collider2D>();
+        Player_anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,5 +31,6 @@ public class NewBehaviourScript : MonoBehaviour
             transform.localScale = new Vector3(-face, transform.localScale.y, transform.localScale.z);
         }
         player_rb.velocity = new Vector2(player_speed * horizontal_num, player_rb.velocity.y);
+        Player_anim.SetFloat("run",Mathf.Abs(player_speed * horizontal_num));
     }
 }
