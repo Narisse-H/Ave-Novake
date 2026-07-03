ee49c3087b88d8ee0c50ed10be6ce1f00bef3ba4
Narisse-H <juzi-ex@outlook.com>
2026-07-03 20:35:12 +0800

    Update Camera Controller.cs

--- Ave Novake-CGJ/Assets/Scripts/Camera Controller.cs
+++ Ave Novake-CGJ/Assets/Scripts/Camera Controller.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject character;
+    public Vector2 ship_pos;
+
+    void Camera_move()//Working in progress...
+    {
+        //
+        ship_pos = character.transform.position;
+        if ((ship_pos.x - transform.position.x) >= 7 || (ship_pos.y - transform.position.y) >= 3.5)
+        {
+            //
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
