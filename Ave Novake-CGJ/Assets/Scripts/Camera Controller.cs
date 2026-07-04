5bd8a1139c95fcebd3ebeeced5e539ffe542ab90
Narisse-H <juzi-ex@outlook.com>
2026-07-04 12:10:09 +0800

    1

--- Ave Novake-CGJ/Assets/Scripts/Camera Controller.cs
+++ Ave Novake-CGJ/Assets/Scripts/Camera Controller.cs
@@ -7,8 +7,19 @@ public class CameraController : MonoBehaviour
 {
     public GameObject character;
     public Vector2 ship_pos;
+<<<<<<< HEAD
     public float thresholdX = 5.0f;
     public float thresholdY = 3.0f;
+=======
+<<<<<<< HEAD
+
+    void Camera_move()//Working in progress...
+    {
+        //
+        ship_pos = character.transform.position;
+        if ((ship_pos.x - transform.position.x) >= 7 || (ship_pos.y - transform.position.y) >= 3.5)
+=======
+>>>>>>> parent of d616af2 (1)
     private float pos_x;
     private float pos_y;
     float smoothSpeed = 3f;
@@ -18,9 +29,14 @@ public class CameraController : MonoBehaviour
         ship_pos = character.transform.position;
         pos_x = (ship_pos.x - transform.position.x);
         pos_y = (ship_pos.y - transform.position.y);
+<<<<<<< HEAD
         Vector3 targetCamPos = transform.position;
 
         if (Mathf.Abs(pos_x) > thresholdX)
+=======
+        if (pos_x >= 7 || pos_x <= -7)
+>>>>>>> parent of 15f0215 (删除原仓库文件，仅保留WindManager项目)
+>>>>>>> parent of d616af2 (1)
         {
             targetCamPos.x = ship_pos.x - thresholdX * Mathf.Sign(pos_x);
         }
